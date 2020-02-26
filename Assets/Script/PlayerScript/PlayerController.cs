750868b0bda42633cebd17f0963a0afa74f25fa7
EthanFife <ethan.z.fif@gmail.com>
2020-02-26 10:06:46 -0800

    Push New Changes
    added movement

---
+++ Assets/Script/PlayerScript/PlayerController.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    private float inputX;
+    private float inputZ;
+    private float speed = 25.0f;
+
+    //abilities
+    public GameObject swordObject;
+    private SwordScript swordScript;
+
+    //ability tracking
+    private bool hasSword = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        inputX = Input.GetAxis("Horizontal");
+        inputZ = Input.GetAxis("Vertical");
+
+        //move in relation to screen
+        transform.Translate(Vector3.right * inputX * speed * Time.deltaTime, Space.World);
+        transform.Translate(Vector3.forward * inputZ * speed * Time.deltaTime, Space.World);
+
+        //attack
+        //space
+        if(Input.GetKeyDown("space") && hasSword)
+        {
+            Debug.Log("stab");
+            swordObject.SetActive(true);
+            //swordScript.Stab();
+        }
+
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("PickUp")){
+            Destroy(other.gameObject);
+
+            swordScript = swordObject.GetComponent<SwordScript>();
+            hasSword = true;
+        }
+    }
+}
---
+++ Assets/Script/WeaponScript/SwordScript.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SwordScript : MonoBehaviour
+{
+    public float stabSpeed = 10.0f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void Stab()
+    {
+        transform.Translate(Vector3.forward * stabSpeed * Time.deltaTime, Space.Self);
+    }
+}
